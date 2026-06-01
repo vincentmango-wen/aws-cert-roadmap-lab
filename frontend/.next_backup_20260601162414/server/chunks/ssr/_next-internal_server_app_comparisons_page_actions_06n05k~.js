6ffd5d3af374ccef6bff1ff27b53f0c4a2ef86c0
vincent.wen <vincentmango.wen@gmail.com>
2026-06-01 17:17:12 +0900

    chore: remove Next.js generated files from git tracking

--- frontend/.next_backup_20260601162414/server/chunks/ssr/_next-internal_server_app_comparisons_page_actions_06n05k~.js
+++
@@ -1,3 +0,0 @@
-module.exports=[93230,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_comparisons_page_actions_06n05k~.js.map
--- frontend/.next_backup_20260601162414/static/ZKFSh39Ecg7fJs4c6E54F/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fquestions\u002F[questionId]","\u002Fterms\u002F[termId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
